eb6789629140862c6aafd4bbba20e66b2f78ad1c
randyajackson <rajackson.dev@gmail.com>
2020-01-13 20:30:55 -0700

    changes

--- artist_page/src/App.tsx
+++ artist_page/src/App.tsx
@@ -1,28 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import * as express from "express";
 import './App.css';
+import logo from './img/logo.png';
+
 
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div className = "topDisplay">
+      <img className = "logo" src = {logo} alt = {logo}></img><br />
+      <span className= "tagLineFirst">featured</span>
+      <span className= "tagLineSecond">creators</span>
     </div>
+    <br/>
+
+  <ul>
+    <li>
+      <img className = "results" src = "https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05586_oj8jfo.jpg" alt="none"></img>
+    </li>
+
+    <li>
+      <img className = "results" src = "https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05459_ziuomy.jpg" alt = "none"></img>
+    </li>
+
+    <li>
+      <img className = "results" src = "https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05513_gfbiwi.jpg" alt="none"></img>
+      
+    </li>
+
+    <li>
+      <img className = "results" src = "https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814784/photostream-photos/DSC05624_f5b2ud.jpg" alt = "none"></img>
+    </li>
+
+    <li>
+      <img className = "results" src = "https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814782/photostream-photos/DSC05559_hu49zx.jpg" alt="none"></img>
+    </li>
+
+    <li>
+      <img className = "results" src = "https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814782/photostream-photos/DSC05482_dtrj02.jpg" alt = "none"></img>
+    </li>
+
+    <li>
+      <img className = "results" src = "https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814782/photostream-photos/DSC05565_dx5rp6.jpg" alt="none"></img>
+    </li>
+
+    <li>
+      <img className = "results" src = "https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814782/photostream-photos/DSC05469_fdxdzx.jpg" alt = "none"></img>
+    </li>
+
+    <li>
+      <img className = "results" src = "https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814781/photostream-photos/DSC05483_dyiuya.jpg" alt = "none"></img>
+    </li>
+
+
+    <li className = "lastRow"></li>
+
+  </ul>
+  </div>
   );
-}
+
+};
 
 export default App;
