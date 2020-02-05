0f1e995c9f3a774d0eb878818c2f0241ca10a931
randyajackson <rajackson.dev@gmail.com>
2020-02-04 20:59:56 -0700

    adding floating text

--- artist_page/src/App.tsx
+++ artist_page/src/App.tsx
@@ -39,36 +39,46 @@ const App: React.FC = () => {
     >
       <div className="grid-item">
         <img src = "http://uberhumor.com/wp-content/uploads/2014/09/lPBJXJl.png" alt="none"></img>
+        <div className="artistName"><p className="artistName">firstname <span className="artistName">lastname</span></p></div>
       </div>
 
       <div className="grid-item">
         <img src = "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/75238076/original/d274960485df2a5b8b3e9960c2c9dda01eb8237d/ask-20-random-strangers-to-choose-your-best-profile-picture.jpg" alt = "none"></img>
+        <div className="artistName"><p className="artistName">firstname <span className="artistName">lastname</span></p></div>
       </div>
 
       <div className="grid-item">
         <img src = "https://cdn-images-1.medium.com/max/1200/1*NpUUls7kjn9JhO4ChjGV7w.png" alt="none"></img>
-        
+        <div className="artistName"><p className="artistName">firstname <span className="artistName">lastname</span></p></div>
       </div>
 
       <div className="grid-item">
         <img src = "https://catchmycar.files.wordpress.com/2011/06/tobey-maguire-very-upset.jpg"  alt = "none"></img>
+        <div className="artistName"><p className="artistName">firstname <span className="artistName">lastname</span></p></div>
       </div>
 
       <div className="grid-item">
         <img src = "https://www.connollysdoitbest.com/Data/ItemImage-160565-2524294.jpg?AutoCrop=1&CropHeight=1440&CropWidth=1440&Resize=Smallest&Revision=0Hd&Timestamp=fkwbVG&Width=1440"  alt="none"></img>
+        <div className="artistName"><p className="artistName">firstname <span className="artistName">lastname</span></p></div>
       </div>
 
       <div className="grid-item">
         <img src = "http://i.imgur.com/6YSV2I3.gif" alt = "none"></img>
+        <div className="artistName"><p className="artistName">firstname <span className="artistName">lastname</span></p></div>
       </div>
 
       <div className="grid-item">
         <img src = "https://avatars0.githubusercontent.com/u/22890813?s=460&v=4"  alt="none"></img>
+        <div className="artistName"><p className="artistName">firstname <span className="artistName">lastname</span></p></div>
       </div>
 
       <div className="grid-item">
         <img src = "https://www.researchgate.net/profile/Jussi_Poikonen/publication/265265707/figure/fig3/AS:669496293478407@1536631763215/Power-delay-profile-of-one-realization-of-a-random-exponentialPDP-WSSUS-channel.png" alt = "none"></img>
+        
+        <div className="artistName"><p className="artistName">firstname <span className="artistName">lastname</span></p></div>
       </div>
+
+      
 
     </Masonry>
   </div>
