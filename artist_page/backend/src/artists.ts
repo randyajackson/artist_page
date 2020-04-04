0f55087b6d433922bfade402539806e313d9a24e
randyajackson <rajackson.dev@gmail.com>
2020-04-04 20:19:49 +0000

    Setup security for mongodb

--- artist_page/backend/src/artists.ts
+++ artist_page/backend/src/artists.ts
@@ -1,6 +1,7 @@
 import * as mongoose from 'mongoose';
+require('dotenv').config();
 
-const uri: string = 'mongodb://localhost/artists';
+const uri: string = process.env.MONGOOSE_URL;
 
 mongoose.connect(uri, (err: any) => {
     if (err) {
@@ -14,17 +15,59 @@ export const ArtistSchema = new mongoose.Schema({
     id: {type:Number, required: false},
     name: {type:String, required: false},
     image: {type:String, required: false},
-    tumblr: {type:String, required: false},
-    twitter: {type:String, required: false}, 
-    soundCloud: {type:String, required: false},
-    bandCamp: {type:String, required: false},
-    spotify:  {type:String, required: false},
-    mixCloud: {type:String, required: false},
-    external: {type:String, required: false},
+
     facebook: {type:String, required: false},
-    youTube: {type:String, required: false},
+    facebook2: {type:String, required: false},
+    facebook3: {type:String, required: false},
+
     instagram: {type:String, required: false},
+    instagram2: {type:String, required: false},
+    instagram3: {type:String, required: false},
+
+    tumblr: {type:String, required: false},
+    tumblr2: {type:String, required: false},
+    tumblr3: {type:String, required: false},
+
+    twitter: {type:String, required: false}, 
+    twitter2: {type:String, required: false},
+    twitter3: {type:String, required: false},
+
+    soundCloud: {type:String, required: false},
+    soundCloud2: {type:String, required: false},
+    soundCloud3: {type:String, required: false},
+
+    residentAdvisor: {type:String, required: false},
+    residentAdvisor2: {type:String, required: false},
+    residentAdvisor3: {type:String, required: false},
+
+    bandCamp: {type:String, required: false},
+    bandCamp2: {type:String, required: false},
+    bandCamp3: {type:String, required: false},
+
+    spotify:  {type:String, required: false},
+    spotify2:  {type:String, required: false},
+    spotify3:  {type:String, required: false},
+
+    mixCloud: {type:String, required: false},
+    mixCloud2: {type:String, required: false},
+    mixCloud3: {type:String, required: false},
+
+    songKick: {type:String, required: false},
+    songKick2: {type:String, required: false},
+    songKick3: {type:String, required: false},
+
+    youTube: {type:String, required: false},
+    youTube2: {type:String, required: false},
+    youTube3: {type:String, required: false},
+
+    discogs: {type:String, required: false},
+    discogs2: {type:String, required: false},
+    discogs3: {type:String, required: false},
+    discogs4: {type:String, required: false},
+    discogs5: {type:String, required: false},
+
     paypal: {type:String, required: false},
+    external: {type:String, required: false},
     email: {type:String, required: false},
     description: {type:String, required: false},
     events: {type:String, required: false},
