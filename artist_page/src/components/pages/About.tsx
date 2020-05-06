067d3a086f9c5d0a42ad9e096ac860f97d0e8d3a
randyajackson <rajackson.dev@gmail.com>
2020-05-06 03:30:58 +0000

    added fade transitions between page clicks

--- artist_page/src/components/pages/About.tsx
+++ artist_page/src/components/pages/About.tsx
@@ -17,48 +17,59 @@ class About extends React.Component<{},any> {
 
     constructor(props: Readonly<{}>){
         super(props);
+
+        this.state = {
+            navBarClicked : 0
+          };
+        
+        this.handleLinkClick = this.handleLinkClick.bind(this);
     
     }
 
+    handleLinkClick = () => {
+
+        this.setState({
+            navBarClicked: 1
+          });
+
+        window.setTimeout(() => { 
+        }, 1000);
+    }
 
     render(){
 
         return(
             <>
-                <NavigationMenu />
-
-                <div className = "featuredTextAbout">
-                <span className= "tagLineSecondAbout">
-                    intrinse 
-                </span>
-                <span className= "tagLineFirstAbout">
-                    <span> </span>
-                    is a platform for finding new music
-                </span>
-                <br/>
-                <span className= "tagLineFirstAboutBody">
-                    <span> </span>
-                    It began as a youtube channel in 2014 that posts full album versions of found albums.
-                    2019 marked the beginning of a livestream radio that plays curated songs from new musical artists alongside a live data dashboard.
+                <div onClick = {this.handleLinkClick}><NavigationMenu /></div>
+                <div className={(this.state.navBarClicked === 0)? "textContainer fadeIn" : "textContainer fadeOut"}>
                     <br/>
-                    The core focus of intrinse is <span className= "tagLineSecondAbout">discovery</span>
-                    <br/>
-                    The core goal of intrinse is to emulate the feeling of flipping through media at your favorite thrift store and finding a new favorite tune.
-                    <br/>
-                    How do we plan to do that?
-                    <br/> 
-                    <span className= "tagLineSecondAbout">exploration</span>
-                    <br/>
-                    Follow along with us as we build and grow:
-                    <br/><br/><br/>
-                    <a href="https://www.youtube.com/c/intrinse" target="_blank" rel="noopener noreferrer" id="intrinsicLinks"><img className="page-header-buttons-image" src = {youtube} alt = {logo}></img></a>
-                    <a href="https://www.instagram.com/intrinse_" target="_blank" rel="noopener noreferrer" id="intrinsicLinks"><img className="page-header-buttons-image" src = {instagram} alt = {logo}></img></a>
-                    <a href="https://soundcloud.com/intrinse" target="_blank" rel="noopener noreferrer" id="intrinsicLinks"><img className="page-header-buttons-image" src = {soundcloud} alt = {logo}></img></a>
-                    <a href="mailto: intrinse.mail@gmail.com" target="_blank" rel="noopener noreferrer" id="intrinsicLinks"><img className="page-header-buttons-image" src = {email} alt = {logo}></img></a>
-                    
-                    
-                </span>
-                
+                    <span className= "tagLineSecondAboutHeader">
+                            intrinse 
+                        </span>
+                        <span className= "tagLineFirstAbout">   
+                            <span> </span>is a platform for finding new music
+                    </span>
+                    <div className = "featuredTextAbout">
+                        <span className= "tagLineFirstAboutBody">
+                            <span> </span>
+                            It began as a youtube channel in 2014 that posts full album versions of found albums.
+                            2019 marked the beginning of a livestream radio that plays curated songs from new musical artists alongside a live data dashboard.
+                            <br/>
+                            The core focus of intrinse is 
+                            <br/>
+                            <span className= "tagLineSecondAbout">discovery</span>
+                            <br/>
+                            The goal of intrinse is to emulate the feeling of flipping through media at your favorite thrift store and finding a new favorite tune.
+                            <br/>
+                            Follow along with us as we build and grow:
+                            <br/><br/>
+                            <a href="https://www.youtube.com/c/intrinse" target="_blank" rel="noopener noreferrer" id="intrinsicLinks"><img className="page-header-buttons-image" src = {youtube} alt = {logo}></img></a>
+                            <a href="https://www.instagram.com/intrinse_" target="_blank" rel="noopener noreferrer" id="intrinsicLinks"><img className="page-header-buttons-image" src = {instagram} alt = {logo}></img></a>
+                            <a href="https://soundcloud.com/intrinse" target="_blank" rel="noopener noreferrer" id="intrinsicLinks"><img className="page-header-buttons-image" src = {soundcloud} alt = {logo}></img></a>
+                            <a href="mailto: intrinse.mail@gmail.com" target="_blank" rel="noopener noreferrer" id="intrinsicLinks"><img className="page-header-buttons-image" src = {email} alt = {logo}></img></a>
+                            </span>
+                            <br/><br/><br/><br/><br/><br/>
+                    </div>
                 </div>
             </>
         );
