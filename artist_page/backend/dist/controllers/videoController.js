8907d23e0c66516f2c4cad6dc165db9181061201
randyajackson <rajackson.dev@gmail.com>
2020-09-20 00:30:15 +0000

    playlist now queries newest added first, work on scripts for importing/updating channels complete

--- artist_page/backend/dist/controllers/videoController.js
+++ artist_page/backend/dist/controllers/videoController.js
@@ -4,7 +4,7 @@ exports.videoByKeyword = void 0;
 const videos_1 = require("./../videos");
 //Get - /youtubeKeywords/keyword returns artist by name likeness
 exports.videoByKeyword = (req, res) => {
-    let youtube_videos = videos_1.default.find({ "video_tags": { $regex: decodeURIComponent(req.params.keyword), $options: 'i' } }, function (err, youtube_videos) {
+    let youtube_videos = videos_1.default.find({ "video_tags": { $regex: decodeURIComponent(req.params.keyword), $options: 'i' } }).sort({ _id: 'descending' }).exec(function (err, youtube_videos) {
         if (err) {
             res.send(err);
         }
@@ -13,4 +13,4 @@ exports.videoByKeyword = (req, res) => {
         }
     });
 };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmlkZW9Db250cm9sbGVyLmpzIiwic291cmNlUm9vdCI6Ii4vc3JjLyIsInNvdXJjZXMiOlsiY29udHJvbGxlcnMvdmlkZW9Db250cm9sbGVyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUNBLHdDQUFpQztBQUVqQyxnRUFBZ0U7QUFDckQsUUFBQSxjQUFjLEdBQUcsQ0FBQyxHQUFZLEVBQUUsR0FBYSxFQUFFLEVBQUU7SUFFeEQsSUFBSSxjQUFjLEdBQUcsZ0JBQU0sQ0FBQyxJQUFJLENBQUUsRUFBQyxZQUFZLEVBQUUsRUFBRSxNQUFNLEVBQUUsa0JBQWtCLENBQUMsR0FBRyxDQUFDLE1BQU0sQ0FBQyxPQUFPLENBQUMsRUFBRSxRQUFRLEVBQUUsR0FBRyxFQUFDLEVBQUUsRUFBRSxVQUFTLEdBQUcsRUFBRSxjQUFjO1FBQzdJLElBQUksR0FBRyxFQUFFO1lBQ0wsR0FBRyxDQUFDLElBQUksQ0FBQyxHQUFHLENBQUMsQ0FBQztTQUNqQjthQUNJO1lBQ0QsR0FBRyxDQUFDLElBQUksQ0FBQyxjQUFjLENBQUMsQ0FBQztTQUM1QjtJQUNMLENBQUMsQ0FBQyxDQUFDO0FBQ1AsQ0FBQyxDQUFDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmlkZW9Db250cm9sbGVyLmpzIiwic291cmNlUm9vdCI6Ii4vc3JjLyIsInNvdXJjZXMiOlsiY29udHJvbGxlcnMvdmlkZW9Db250cm9sbGVyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUNBLHdDQUFpQztBQUVqQyxnRUFBZ0U7QUFDckQsUUFBQSxjQUFjLEdBQUcsQ0FBQyxHQUFZLEVBQUUsR0FBYSxFQUFFLEVBQUU7SUFFeEQsSUFBSSxjQUFjLEdBQUcsZ0JBQU0sQ0FBQyxJQUFJLENBQUUsRUFBQyxZQUFZLEVBQUUsRUFBRSxNQUFNLEVBQUUsa0JBQWtCLENBQUMsR0FBRyxDQUFDLE1BQU0sQ0FBQyxPQUFPLENBQUMsRUFBRSxRQUFRLEVBQUUsR0FBRyxFQUFDLEVBQUUsQ0FBQyxDQUFDLElBQUksQ0FBQyxFQUFDLEdBQUcsRUFBRyxZQUFZLEVBQUMsQ0FBQyxDQUFDLElBQUksQ0FBQyxVQUFTLEdBQVEsRUFBRSxjQUFtQjtRQUN2TCxJQUFJLEdBQUcsRUFBRTtZQUNMLEdBQUcsQ0FBQyxJQUFJLENBQUMsR0FBRyxDQUFDLENBQUM7U0FDakI7YUFDSTtZQUNELEdBQUcsQ0FBQyxJQUFJLENBQUMsY0FBYyxDQUFDLENBQUM7U0FDNUI7SUFDTCxDQUFDLENBQUMsQ0FBQztBQUNQLENBQUMsQ0FBQyJ9
--- artist_page/backend/src/controllers/videoController.ts
+++ artist_page/backend/src/controllers/videoController.ts
@@ -4,7 +4,7 @@ import Videos from './../videos';
 //Get - /youtubeKeywords/keyword returns artist by name likeness
 export let videoByKeyword = (req: Request, res: Response) => {
 
-    let youtube_videos = Videos.find( {"video_tags": { $regex: decodeURIComponent(req.params.keyword), $options: 'i'} } ,function(err, youtube_videos) {
+    let youtube_videos = Videos.find( {"video_tags": { $regex: decodeURIComponent(req.params.keyword), $options: 'i'} }).sort({_id : 'descending'}).exec(function(err: any, youtube_videos: any) {
         if (err) {
             res.send(err);
         }
